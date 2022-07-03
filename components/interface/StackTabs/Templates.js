5e869f6bf71db5197dad5018d4a00aa904495560
indra <adityaindra135@gmail.com>
2022-07-03 10:25:46 +0700

    Add code editor for stack templates

--- components/interface/StackTabs/Templates.js
+++ components/interface/StackTabs/Templates.js
@@ -1,7 +1,12 @@
 import {useRouter} from "next/router";
 import useSWR from "swr";
-import {Spinner} from "react-bootstrap";
+import {Spinner, Row, Col} from "react-bootstrap";
 import api from "../../../utils/api";
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-yaml";
+import "prismjs/themes/prism-solarizedlight.css";
 
 const fetcher = url => api.get(url).then(res => res.data.data)
 
@@ -22,9 +27,19 @@ const Templates = () => {
     return (
         <>
             <br />
-            <textarea rows={50} cols={100} disabled>
-                {data}
-            </textarea>
+            <Row>
+                <Col lg={12}>
+                    <Editor
+                        value={data}
+                        highlight={data => highlight(data, languages.yaml)}
+                        padding={10}
+                        style={{
+                            fontFamily: '"Fira code", "Fira Mono", monospace',
+                            fontSize: 12,
+                        }}
+                    />
+                </Col>
+            </Row>
         </>
     );
 };
